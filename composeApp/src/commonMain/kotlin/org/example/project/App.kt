65bb82229f3896bcbf9b304840a619978ee66615
Suraj Kumar <sk96.uk@gmail.com>
2025-11-16 11:59:16 +0000

    feat: pass Database instance to App.kt

--- composeApp/src/commonMain/kotlin/org/example/project/App.kt
+++ composeApp/src/commonMain/kotlin/org/example/project/App.kt
@@ -1,10 +1,13 @@
 package org.example.project
 
 import androidx.compose.runtime.*
+import org.example.project.database.AppDatabase
 import org.jetbrains.compose.ui.tooling.preview.Preview
 
 @Composable
 @Preview
-fun App() {
+fun App(
+    database: AppDatabase
+) {
 
 }
--- composeApp/src/iosMain/kotlin/org/example/project/MainViewController.kt
+++ composeApp/src/iosMain/kotlin/org/example/project/MainViewController.kt
@@ -1,5 +1,12 @@
 package org.example.project
 
 import androidx.compose.ui.window.ComposeUIViewController
+import org.example.project.database.getDatabaseBuilder
 
-fun MainViewController() = ComposeUIViewController { App() }
+fun MainViewController() {
+    val database = getDatabaseBuilder().build()
+
+    ComposeUIViewController {
+        App(database = database)
+    }
+}
